a049d8701ec82e53c445e8cdf6f65b1adab2981f
arelymartinez16 <64700714+arelymartinez16@users.noreply.github.com>
2023-07-22 23:52:35 +0000

    Created Node class

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node {
+  private int val;
+  private int next;
+  
+  public Node(int val) {
+    this.val = val;
+    this.next = null;
+  }
+
+  public static void main(String[] args) {
+  }
+}
