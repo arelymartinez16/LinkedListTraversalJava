6ee4a22ab51e99733a2758f2270854aedc794c59
arelymartinez16 <64700714+arelymartinez16@users.noreply.github.com>
2023-07-23 00:07:42 +0000

    Created printLinkedList method in the Node class and tested the class in the main method

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world and GitHub!");
+    Node a = new Node(1);
+    Node b = new Node(2);
+    Node c = new Node(3);
+    Node d = new Node(4);
+
+    // Node a is pointing to b
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    // 1 -> 2 -> 3 -> 4 -> null (bc of the default value in the constructor but it's the same as nothing)
+
+    Node.printLinkedList(a);
   }
 }
--- Node.java
+++ Node.java
@@ -1,22 +1,22 @@
 public class Node {
-  private int val;
-  private int next;
+  public int val;
+  public Node next;
   
   public Node(int val) {
     this.val = val;
     this.next = null;
   }
 
-  public static void main(String[] args) {
-    Node a = new Node(1);
-    Node b = new Node(2);
-    Node c = new Node(3);
-    Node d = new Node(4);
+  public static void printLinkedList(Node head) {
+    Node current = head;
 
-    // Node a is pointing to b
-    a.next = b;
-    b.next = c;
-    c.next = d;
-    // 1 -> 2 -> 3 -> 4 -> null (bc of the default value in the constructor but it's the same as nothing)
+    while (current != null) {
+      System.out.println(current.val);
+      current = current.next;
+    }
+  }
+
+  public static void main(String[] args) {
+    
   }
 }
